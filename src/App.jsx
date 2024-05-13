7586715096767d843746d729a610c2b32395e680
Mohammed Parves <mdperrfan@gmail.com>
2024-05-13 20:32:46 +0600

    Added Todo componentsfunctionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
 import './Style.css'
 import Navbar from './Componenst/Navbar'
 import Footer from './Componenst/Footer'
 import Todo from './Componenst/Todo'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <div className="app-body">
     <Navbar/>
     <Todo/>
     <Footer/>
+    </div>
+
     </>
   )
 }
--- src/Componenst/Todo.jsx
+++ src/Componenst/Todo.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import '../Style.css'
-export default function Todo() {
-  return (
-    <div>
-        <div className="todo-section">
-        <h1>Todo List</h1>
-        <ul>
-            <li>
+import { useState } from 'react';
 
-            </li>
-        </ul>
-        </div>
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleOnchange = event => {
+    setInputValue(event.target.value);
+  };
+  const handleAddtodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+    }
+    setInputValue('');
+  };
+  const handleDelete = index => {
+    let newTodo = todos.filter((_, i) => i !== index);
+    setTodos(newTodo);
+  };
+  return (
+    <>
+    <div className="container todo-section">     
+    <div class="input-group mb-3">
+      <input type="text"value={inputValue} className="form-control"  aria-describedby="button-addon2"onChange={handleOnchange}/>
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2"onClick={handleAddtodo}>ADD</button>
     </div>
-  )
+      <h1 className='my-5'>Todo List</h1>
+      <ul>
+          {todos.length === 0 ? (
+            <div className="mssg">
+              <h4 style={{color:"red"}}>No todos</h4>
+            </div>
+          ) : (
+            todos.map((todo, index) => (
+              <li key={index}>
+                {todo}
+                <button className="btn btn-danger mx-4" onClick={() => handleDelete(index)}>Delete</button>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    </>
+  );
 }
+export default Todo;
